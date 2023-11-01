cb6a082257d0e927123f054a5ab65bf314cd076e
lambojac <you@example.com>
2023-11-01 17:47:23 +0100

    url

--- config/alllowedOrigins.js
+++ config/alllowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'frontend-bn37u4b6k-adamokehinde068-gmailcom.vercel.app'
 ]
 
 export default allowedOrigins; 
